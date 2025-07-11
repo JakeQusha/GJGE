97f5ae1bd955c1001859797e9d4748b865138202
brua11a <adamwwsluzbowy@gmail.com>
2025-07-11 13:44:04 +0200

    scene macros

--- engine/inc/scene.hpp
+++ engine/inc/scene.hpp
@@ -3,6 +3,18 @@
 #include "imgui.h"
 #include "assets/asset_manager.hpp"
 #include <functional>
+#define GEN_SCENES(name) inline void name(ge::SceneManager& scene_manager)
+#define SCENE(name)                                                                                                    \
+    extern void gen_##name##_scene(const ge::SceneManager&);                                                           \
+    gen_##name##_scene(scene_manager);
+#define SCENE_DEF(name)                                                                                                \
+    static void setup(entt::registry&, ge::AssetManager&);                                                             \
+    static void update(entt::registry&, ge::AssetManager&);                                                            \
+    void gen_##name##_scene(const ge::SceneManager& scene_manager) { scene_manager.make_scene(#name, setup, update); }
+#define SETUP                                                                                                          \
+    static void setup([[maybe_unused]] entt::registry& registry, [[maybe_unused]] ge::AssetManager& asset_manager)
+#define UPDATE                                                                                                         \
+    static void update([[maybe_unused]] entt::registry& registry, [[maybe_unused]] ge::AssetManager& asset_manager)
 
 namespace ge {
 namespace comp {

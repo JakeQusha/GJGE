a1cb5109eeb29131cfd73392fd1f1e25c8f19c8f
JakeQusha <mateuszjakoniuk@outlook.com>
2025-07-28 21:11:46 +0200

    added operators for vec3

--- engine/inc/raylib_operators.hpp
+++ engine/inc/raylib_operators.hpp
@@ -1,13 +1,23 @@
 #pragma once
 #include <raylib.h>
 
-inline Vector2 operator+(const Vector2& l, const Vector2& r) { return {l.x + r.x, l.y + r.y}; }
-inline Vector2 operator-(const Vector2& l, const Vector2& r) { return {l.x - r.x, l.y - r.y}; }
-inline Vector2 operator*(const Vector2& l, const Vector2& r) { return {l.x * r.x, l.y * r.y}; }
-inline Vector2 operator/(const Vector2& l, const Vector2& r) { return {l.x / r.x, l.y / r.y}; }
+inline auto operator+(const Vector2& l, const Vector2& r) -> Vector2 { return {l.x + r.x, l.y + r.y}; }
+inline auto operator-(const Vector2& l, const Vector2& r) -> Vector2 { return {l.x - r.x, l.y - r.y}; }
+inline auto operator*(const Vector2& l, const Vector2& r) -> Vector2 { return {l.x * r.x, l.y * r.y}; }
+inline auto operator/(const Vector2& l, const Vector2& r) -> Vector2 { return {l.x / r.x, l.y / r.y}; }
 
-inline Vector2 operator+(const Vector2& l, const float& r) { return {l.x + r, l.y + r}; }
-inline Vector2 operator-(const Vector2& l, const float& r) { return {l.x - r, l.y - r}; }
-inline Vector2 operator*(const Vector2& l, const float& r) { return {l.x * r, l.y * r}; }
-inline Vector2 operator/(const Vector2& l, const float& r) { return {l.x / r, l.y / r}; }
+inline auto operator+(const Vector2& l, const float& r) -> Vector2 { return {l.x + r, l.y + r}; }
+inline auto operator-(const Vector2& l, const float& r) -> Vector2 { return {l.x - r, l.y - r}; }
+inline auto operator*(const Vector2& l, const float& r) -> Vector2 { return {l.x * r, l.y * r}; }
+inline auto operator/(const Vector2& l, const float& r) -> Vector2 { return {l.x / r, l.y / r}; }
+
+inline auto operator+(const Vector3& l, const Vector3& r) -> Vector3 { return {l.x + r.x, l.y + r.y, l.z + r.z}; }
+inline auto operator-(const Vector3& l, const Vector3& r) -> Vector3 { return {l.x - r.x, l.y - r.y, l.z - r.z}; }
+inline auto operator*(const Vector3& l, const Vector3& r) -> Vector3 { return {l.x * r.x, l.y * r.y, l.z * r.z}; }
+inline auto operator/(const Vector3& l, const Vector3& r) -> Vector3 { return {l.x / r.x, l.y / r.y, l.z / r.z}; }
+
+inline auto operator+(const Vector3& l, const float& r) -> Vector3 { return {l.x + r, l.y + r, l.z + r}; }
+inline auto operator-(const Vector3& l, const float& r) -> Vector3 { return {l.x - r, l.y - r, l.z - r}; }
+inline auto operator*(const Vector3& l, const float& r) -> Vector3 { return {l.x * r, l.y * r, l.z * r}; }
+inline auto operator/(const Vector3& l, const float& r) -> Vector3 { return {l.x / r, l.y / r, l.z / r}; }
 

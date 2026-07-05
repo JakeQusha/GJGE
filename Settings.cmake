c5f65aa5f7c9bca909447f11e2a31cfedb0e544b
JakeQusha <mateuszjakoniuk@outlook.com>
2026-07-05 13:40:01 +0200

    removed box2d

--- Settings.cmake
+++ Settings.cmake
@@ -1,4 +1,3 @@
 option(GJGE_FEATURE_NETWORKING "Enable support for networking" OFF) # requires ENet as external dependency TODO: not implemented yet
-option(GJGE_FEATURE_BOX2D "Enable support for BOX2D physics engine" OFF) # requires Box2D as external dependency TODO: under development
 option(GJGE_DEV_TOOLS "Enable inspector and other stuff" ON)
 option(GJGE_DEV_CONSOLE "" ON)
